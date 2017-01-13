5f95541fe319548de021b671de90922dd1ddd678
Lluís Padró <padro@lsi.upc.edu>
2017-01-13 17:22:25 +0100

    paths in ASR config file

--- src/libfreeling/asr/asr.cc
+++ src/libfreeling/asr/asr.cc
@@ -64,7 +64,7 @@ namespace freeling {
     if (not cfg.open(configFile))
       ERROR_CRASH(L"Error opening file " + configFile);
 
-    string pathToData = util::wstring2string(configFile.substr(0, configFile.rfind(L"config/")));
+    string pathToData = util::wstring2string(configFile.substr(0,configFile.find_last_of(L"/\\")+1));
 
     // Variables to be set by the config file
     string fst_filename;
