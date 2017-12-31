edd6576eba595dce4168746732de4fb85abd8228
Lluís Padró <padro@cs.upc.edu>
2017-12-31 18:10:45 +0100

    adapting to Cmake

--- src/utilities/installation/dicc2phon.cc
+++ src/utilities/installation/dicc2phon.cc
@@ -43,35 +43,75 @@
 ////////////////////////////////////////////////////////////////
 
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <cstdlib>
 
-#include "freeling/morfo/traces.h"
 #include "freeling/morfo/util.h"
+#include "freeling/morfo/configfile.h"
 #include "freeling/morfo/phonetics.h"
 
 using namespace std;
 using namespace freeling;
 
+void read_config(const wstring &altsFile, wstring &phdic, wstring &phrules) {
+  
+  wstring path=altsFile.substr(0,altsFile.find_last_of(L"/\\")+1);
+  
+  enum sections {GENERAL};
+  config_file cfg(true);
+  cfg.add_section(L"General",GENERAL);
+  if (not cfg.open(altsFile)) {
+    wcerr << "Error opening file " << altsFile << endl;
+    exit(-1);
+  }
+    
+  // load ortographic alternatives configuration
+  wstring line;
+  while (cfg.get_content_line(line)) {
+    wistringstream sin;
+    sin.str(line);
+    wstring key,value;
+    sin>>key>>value;
+    
+    switch (cfg.get_section()) {
+      // Read <General> section contents
+    case GENERAL: {
+      if (key==L"PhoneticDictionary") phdic = util::absolute(value,path); 
+      else if (key==L"PhoneticRules") phrules = util::absolute(value,path); 
+      break;
+    }
+    default: break;
+    }
+  }
+  cfg.close(); 
+}
+
+
 int main(int argc, char *argv[]){
 
   util::init_locale(L"default");
 		
-  if (argc<2) { 
-    wcout << L"You need to specify a phonetics configuration file." << endl; 
+  if (argc!=3) { 
+    wcout << L"Usage: "<< util::string2wstring(argv[0]) << " dicc.src alternatives-phon" << endl; 
     exit(1);
   }
 
+  // get phonetic rules file name and target dictionary name
+  wstring dic_file, ph_file;
+  read_config(util::string2wstring(argv[2]), dic_file, ph_file);
+  
   // create phonetic encoder
-  wstring phFile = util::string2wstring(string(argv[1]));	
-  phonetics ph(phFile);
+  phonetics ph(ph_file);
   
   // remember which words originated each sound
   map<wstring,wstring> orthogr;
+ 
+  std::wifstream dicc;
+  util::open_utf8_file(dicc, util::string2wstring(argv[1]));
 
-  wstring line;
-  wstring word; 
-  while (getline(wcin,line)) {
+  wstring line, word; 
+  while (getline(dicc,line)) {
 
     // read the word form the dictionary, ignoring the rest
     wistringstream sin; sin.str(line); sin>>word;
@@ -80,14 +120,15 @@ int main(int argc, char *argv[]){
 
     // store phonetic form, and how it is spelled, accounting for homophones
     map<wstring,wstring>::iterator p=orthogr.find(sound);
-    if (p==orthogr.end()) 
-      orthogr.insert(make_pair(sound,word));
-    else 
-      p->second = p->second+L" "+word;
+    if (p==orthogr.end()) orthogr.insert(make_pair(sound,word));
+    else p->second = p->second+L" "+word;
   }
+  dicc.close();
   
   // dump built dictionary to output
+  std::wofstream res;
+  util::open_utf8_file(res, dic_file);
   for (map<wstring,wstring>::const_iterator phon = orthogr.begin(); phon!=orthogr.end(); ++phon) 
-    wcout<<phon->first<<L" "<<phon->second<<endl;
-  
+    res << phon->first << L" " << phon->second<<endl;
+  res.close();
 }
---
+++ src/utilities/installation/gz-cat.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+#include <boost/filesystem.hpp>
+
+using namespace std;
+namespace fs = boost::filesystem;
+
+int main(int argc, char *argv[]) {
+
+  if (argc!=3) {
+    cerr << "Usage:  " << string(argv[0]) << " /path/to/parameters.XX.gz.aa /path/to/result-file" << endl;
+    exit(-1);
+  }
+
+  // input filename
+  fs::path aafile = fs::path(argv[1]);
+  if (not is_regular_file(aafile)) {
+    cerr << "file " << aafile << " not found" << endl;
+    exit(-1);
+  }
+
+  // result file name
+  fs::path gzfile = fs::path(argv[2]); 
+
+  fs::path base = aafile.filename().stem();   // get basename without ".aa"
+  fs::path dir = aafile.parent_path(); // get directory
+  
+  // open destination file for writting
+  ofstream dest(gzfile.c_str(), ios::out | ios::binary); 
+
+  // get files in the same directory than input
+  vector<fs::path> gzparts;
+  copy(fs::directory_iterator(dir), fs::directory_iterator(), back_inserter(gzparts));  
+  sort(gzparts.begin(), gzparts.end());
+
+  // add files in the input directory with the same prefix than given .aa file
+  const size_t LEN=65536;
+  for (vector<fs::path>::const_iterator p=gzparts.begin(); p!=gzparts.end(); ++p) {
+    if (p->filename().stem() == base) {
+      // the file matches the base name, append it to dest
+      char buffer[LEN];
+      ifstream src(p->c_str(), ios::in | ios::binary);
+      while (!src.eof()) {
+	src.read(buffer, LEN);
+	dest.write(buffer, src.gcount());
+      }
+      src.close();
+    }
+  }
+  dest.close();
+
+}
